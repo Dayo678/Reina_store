ac53be600ade2fe8c5f4aff12ebc5a41e40ac5ab
dayo <dayoml@yahoo.com>
2023-06-23 21:06:32 -0400

    aks cluster provision

--- aks-cluster/virtual-network.tf
+++ aks-cluster/virtual-network.tf
@@ -14,4 +14,7 @@ resource "azurerm_subnet" "aks-vnet-subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.aks-vent.name
   address_space     = ["10.0.1.0/16"]
+  depends_on = [
+    azurerm_virtual_network.aks-vent
+  ]
 }
